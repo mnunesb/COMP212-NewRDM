ad5e3107faac894bf8499f1db3f8d3b625a5ede2
Marcello Bernardes <mnunesbernardes@gmail.com>
2022-09-12 10:55:38 -0400

    Added third party libraries

---
+++ scripts/app.js
@@ -0,0 +1,12 @@
+//IIFE -- immediately invoked function expression
+(function()
+{
+
+    function Start()
+    {
+        console.log("App Started!");
+    }
+
+    window.addEventListener("load", Start);
+    
+})();
